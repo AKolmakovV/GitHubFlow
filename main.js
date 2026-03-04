3a75b2078b8ab381b6236508e0ef0e463a96574a
Anton Kolmakov <akolmakov-v@yandex.ru>
2026-03-04 09:01:52 +0300

    Новая функция - ответ, на firstMessage

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ function firstMessage() {
     console.log('hello wold');
 }
 
+function secondMessage() {
+    console.log('hi!');
+}
+
 function main() {
     firstMessage();
+    secondMessage();
 }
